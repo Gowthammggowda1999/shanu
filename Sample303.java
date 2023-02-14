0efb5f66479325de3781fd9d21f7ed1ed9678d8f
Gowthammggowda1999 <mggowthamgowda@gmail.com>
2023-02-14 19:08:19 +0530

    init b

---
+++ Sample303.java
@@ -0,0 +1,13 @@
+class  Sample303
+{
+	public static void main(String[] args) 
+	{
+		char ch='a';
+		int i=1;
+		for(i=1;i<=4;i++);
+		{
+		System.out.println(ch++ +""+ch++ +""+ ch++ +i );
+		}
+		ch++;
+	}
+}
