3facc3a11d97b064c6c46c1f5e8af971e20af731
Gowthammggowda1999 <mggowthamgowda@gmail.com>
2023-02-14 19:03:32 +0530

    hii

---
+++ Sample304.java
@@ -0,0 +1,19 @@
+class Sample304
+{
+	public static void main(String[] args) 
+	{
+	 x =Sample().area(7);
+	System.out.println(x);
+	}
+}
+class Sample
+{
+
+	public static void area(int r)
+	{
+		final double pi=3.14;
+	    result=pi*r*r;
+		System.out.println(result);
+		
+	}
+}
